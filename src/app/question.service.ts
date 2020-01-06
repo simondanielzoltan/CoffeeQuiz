07cfdf88cc6b27d9b21285e56e55c622aaa57c2c
Simon Dániel <badsimon@hotmail.hu>
2020-01-06 15:56:20 +0100

    add get random question to service

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Question } from './question';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRandomQuestion(): Observable<Question>{
+    return this.http.get<Question>('http://jservice.io/api/random');
+  }
 }
