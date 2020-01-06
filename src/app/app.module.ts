3142c9421939b4f767aa36bb07ac8a85485df874
Simon Dániel <badsimon@hotmail.hu>
2020-01-06 18:39:17 +0100

    add route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CategoriesComponent } from './categories/categories.component';
+
+
+const routes: Routes = [
+  { path: '', component: QuestionComponent },
+  { path: 'categories', component:CategoriesComponent },
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
     FormsModule,
     HttpClientModule
   ],
