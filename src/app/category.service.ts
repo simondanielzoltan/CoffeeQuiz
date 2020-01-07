9bb9641f270b282effb142444e25537b8988ccea
Simon Dániel <badsimon@hotmail.hu>
2020-01-07 13:47:16 +0100

    add get categories to service

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category } from './category';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  private apiUrl = "http://jservice.io/api";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+
+  getCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>(`${this.apiUrl}/categories/?count=10`);
+  }
 }
